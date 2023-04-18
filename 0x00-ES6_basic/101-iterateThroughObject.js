b1690556b7dc3bf57279209a14ed582a7c9e4f59
NegasiAbe <negasiabebe@gmail.com>
2023-04-18 19:10:05 +0200

    solution added

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,11 @@
+
+function iterateThroughObject(reportWithIterator) {
+    for (let key of reportWithIterator) {
+      console.log(`Category: ${key}`);
+      let employees = reportWithIterator[key];
+      for (let employee of employees) {
+        console.log(`Employee: ${employee}`);
+      }
+    }
+  }
+  
