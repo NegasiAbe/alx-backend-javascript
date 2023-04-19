b2bf6c3f677a7d2634bf6462beefe659582944cd
NegasiAbe <negasiabebe@gmail.com>
2023-04-19 20:24:04 +0200

    solution updated

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,14 +1,4 @@
 export default function uploadPhoto(fileName) {
-    return new Promise((resolve, reject) => {
-      // Simulate processing of file by waiting for 2 seconds
-      setTimeout(() => {
-        // Check if file name ends with ".jpg" or ".png"
-        if (fileName.endsWith('.jpg') || fileName.endsWith('.png')) {
-          resolve(`File ${fileName} uploaded successfully`);
-        } else {
-          reject(new Error(`$fileName cannot be processed`));
-        }
-      }, 2000);
-    });
-  }
-  
+    
+    return Promise.reject(Error(`${filename} is incorrect format`));
+}
