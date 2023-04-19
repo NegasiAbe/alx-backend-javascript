167a10ed457b9be3b726fa3bffdcc628c82d0e88
NegasiAbe <negasiabebe@gmail.com>
2023-04-19 20:42:47 +0200

    solution added

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+export default function divideFunction(numerator, denominator) {
+    if (denominator === 0) {
+      throw new Error('Cannot divide by 0');
+    }
+  
+    return numerator / denominator;
+  }
+  export default function divideFunction(numerator, denominator) {
+    if (denominator === 0) {
+      throw new Error('Cannot divide by 0');
+    }
+  
+    return numerator / denominator;
+  }
+    
