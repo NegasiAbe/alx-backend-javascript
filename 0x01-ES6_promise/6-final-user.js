a0cd2f3f1b8da340ff3915ff89f77cf834843645
NegasiAbe <negasiabebe@gmail.com>
2023-04-21 20:11:57 +0200

    updated

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,11 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-/*export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
   
-  return Promise.allSettled([promise1, promise2]).then(results => {
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then(results => {
         const result = [];
         values.forEach((element) => {
           if (element.status === 'fulfilled') {
@@ -16,21 +16,6 @@ import uploadPhoto from './5-photo-reject.js';
         });
         return result;
       });
-    }*/
+    }
 
-    export default function handleProfileSignup(firstName, lastName, fileName) {
-        const Promise1 = signUpUser(firstName, lastName);
-        const promise2 = uploadPhoto(fileName);
-      
-        return Promise.allSettled([Promise1, Promise2]).then((values) => {
-          const result = [];
-          values.forEach((element) => {
-            if (element.status === 'fulfilled') {
-              result.push({ status: element.status, value: element.value });
-            } else {
-              result.push({ status: element.status, value: `${element.reason}` });
-            }
-          });
-          return result;
-        });
-      }
+   
