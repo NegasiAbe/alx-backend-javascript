5ef135fc530c3890f3ce872b70f3cd5c31926e1b
NegasiAbe <negasiabebe@gmail.com>
2023-04-21 18:16:49 +0200

    update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+/*export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
   
@@ -16,4 +16,21 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
         });
         return result;
       });
-    }
+    }*/
+
+    export default function handleProfileSignup(firstName, lastName, fileName) {
+        const signUpUserPromise = signUpUser(firstName, lastName);
+        const uploadPhotoPromise = uploadPhoto(fileName);
+      
+        return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+          const result = [];
+          values.forEach((element) => {
+            if (element.status === 'fulfilled') {
+              result.push({ status: element.status, value: element.value });
+            } else {
+              result.push({ status: element.status, value: `${element.reason}` });
+            }
+          });
+          return result;
+        });
+      }
