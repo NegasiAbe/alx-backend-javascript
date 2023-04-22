2d90967c03a66bd472ec5700c79f39a87ecef5b7
NegasiAbe <negasiabebe@gmail.com>
2023-04-22 23:37:21 +0200

    solution added

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,19 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpUserPromise = signUpUser(firstName, lastName);
   const uploadPhotoPromise = uploadPhoto(fileName);
-  
-  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then(results => {
-        const result = [];
-        values.forEach((element) => {
-          if (element.status === 'fulfilled') {
-            result.push({ status: element.status, value: element.value });
-          } else {
-            result.push({ status: element.status, value: `${element.reason}` });
-          }
-        });
-        return result;
-      });
-    }
 
-   
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
+    });
+    return result;
+  });
+}
