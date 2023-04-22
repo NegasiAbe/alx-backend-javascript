fdd1d0acaa36a3aa27dda72db9bd620e75e6a127
NegasiAbe <negasiabebe@gmail.com>
2023-04-22 23:55:50 +0200

    updated

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,14 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
-async function asyncUploadUser(userData, photoData) {
+export default async function asyncUploadUser() {
+  let res = {};
+
   try {
-    const [photo, user] = await Promise.all([
-      uploadPhoto(photoData),
-      createUser(userData),
-    ]);
-    return { photo, user };
-  } catch (error) {
-    console.error(error);
-    return { photo: null, user: null };
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
   }
+  return res;
 }
