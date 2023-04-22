a9c50a2e1b80d5d2b5318cfd888f29043fb4c12f
NegasiAbe <negasiabebe@gmail.com>
2023-04-22 23:31:56 +0200

    updated

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,12 @@
-import { uploadPhoto, createUser } from './utils.js';
+
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(results => {
-      const [photo, user] = results;
-      console.log(`Body ${user.firstName} ${user.lastName}`);
-    })
-    .catch(error => {
-      console.error('Signup system offline');
-    });
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  return Promise.all([photo, user]).then((res) => {
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
