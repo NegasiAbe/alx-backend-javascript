6e94bb4e4aa803d81115b278958b1c0d6e050d2c
NegasiAbe <negasiabebe@gmail.com>
2023-04-25 20:21:07 +0200

    updated

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,9 @@
-export default function cleanSet(set, startString) {
-    const filteredValues = [];
-    for (const value of set) {
-      if (value.startsWith(startString)) {
-        filteredValues.push(value.substring(startString.length));
-      }
+function cleanSet(set, startString) {
+  const filteredValues = [];
+  for (const value of set) {
+    if (startString && value.startsWith(startString)) {
+      filteredValues.push(value.substring(startString.length));
     }
-    return filteredValues.join('-');
   }
-  
+  return filteredValues.join('-');
+}
