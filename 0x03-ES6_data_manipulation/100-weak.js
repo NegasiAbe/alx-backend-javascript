62ea1fdfdbcaccb022f64b4b8cd2b1ac71d1a360
NegasiAbe <negasiabebe@gmail.com>
2023-04-25 20:42:12 +0200

    update

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,17 +1,8 @@
 export const weakMap = new WeakMap();
 
-export  function queryAPI(endpoint) {
-  if(typeof endpoint !== 'object' || endpoint === null) {
-    throw new Error('Invalid endpoint');
-  }
-
-  const count = weakMap.get(endpoint) || 0;
-  if(count >= 5) {
-    throw new Error('Endpoint load is high');
-  }
-
-  weakMap.set(endpoint, count + 1);
-
-  // API query code goes here
+export function queryAPI(endpoint) {
+  let called = 0;
+  if (weakMap.get(endpoint)) called = weakMap.get(endpoint);
+  weakMap.set(endpoint, called + 1);
+  if (called + 1 >= 5) throw new Error('over loaded');
 }
-export { weakMap, queryAPI };
