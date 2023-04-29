4900bd99c5653f26cc9669188f1806d2595a53e5
NegasiAbe <negasiabebe@gmail.com>
2023-04-30 00:25:17 +0200

    updated

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,32 +1,26 @@
 export default class Airport {
-    constructor(name, code) {
-      this._name = name;
-      this._code = code;
-    }
-  
-    // Getter for name
-    get name() {
-      return this._name;
-    }
-  
-    // Setter for name
-    set name(name) {
-      this._name = name;
-    }
-  
-    // Getter for code
-    get code() {
-      return this._code;
-    }
-  
-    // Setter for code
-    set code(code) {
-      this._code = code;
-    }
-  
-    // Default string description of the class
-    toString() {
-      return this._code;
-    }
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
   }
-  
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
